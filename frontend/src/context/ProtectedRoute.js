8a2a8d9988a86da280a0512e93047f708ec38507
poojadayal <poojaadayal@gmail.com>
2024-07-09 02:14:46 +0530

    fifth commit
    Signed-off-by: poojadayal <poojaadayal@gmail.com>

---
+++ frontend/src/context/ProtectedRoute.js
@@ -0,0 +1,9 @@
+// import { useContext } from "react";
+// import { Navigate, Outlet } from "react-router-dom";
+// import { AuthContext } from "./AuthProvider";
+
+// export const PrivateRoute = () => {
+//   const { isLoggedIn } = useContext(AuthContext);
+
+//   return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
+// };
